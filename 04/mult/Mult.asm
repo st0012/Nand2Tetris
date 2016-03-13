ee2cb5f2d4b85dc1287bae332a51b3b9e16207fc
Stan Lo <a22301613@yahoo.com.tw>
2016-03-13 19:56:34 +0800

    Project-4's mult done

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -7,3 +7,52 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+// for (i=0, i<R1, i++) do
+//   r = r + R0
+// end
+// R2 = r
+
+@i
+M = 0 // define i
+
+@r
+M = 0 // define r
+
+(LOOP)
+@i
+D=M
+
+@R0
+D=D-M
+
+@STORE
+D;JEQ // jump to store result if i > R0
+
+@R1
+D=M
+@r
+M=D+M // result = result + R1
+
+@i
+M=M+1 // i++
+
+@LOOP
+0;JMP
+
+(STORE)
+@r
+D=M
+@R2
+M=D // R2 = final r
+
+(END)
+@END
+0;JMP
+
+
+
+
+
+
+
+
