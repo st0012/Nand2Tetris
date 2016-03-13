fc425d924bcda0549d5de26832aa082b69d5d684
Stan Lo <a22301613@yahoo.com.tw>
2016-03-13 20:43:39 +0800

    Fill.asm done

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -9,3 +9,65 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+
+@i
+M = 0
+
+@8192
+D=A
+@n
+M = D // define screen loop number
+
+@SCREEN
+D=A
+@addr
+M=D
+
+
+@KBD
+D=M
+@CLEAN_SCREEN
+D;JEQ
+
+(BLACK_SCREEN)
+@screen
+M=-1
+
+@LOOP
+0;JMP
+
+(CLEAN_SCREEN)
+@screen
+M=0
+
+@LOOP
+0;JMP
+
+(LOOP)
+@i
+D=M
+@n
+D=D-M
+@END
+D;JEQ
+
+@screen
+D=M
+
+@addr
+A=M
+M=D
+
+@addr
+M=M+1 // increase address
+
+@i
+M=M+1 // i++
+
+@LOOP
+0;JMP
+
+
+(END)
+@R0
+0;JMP
